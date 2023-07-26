38882acc08ef6ed51edf37d69a3b85e824164388
emmanuelmbithi <embithimuthianigmail.com>
2023-07-26 20:08:01 +0300

    just app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 //import ButtonOnClick from "./Creating-and-nesting-components/ButtonOnClick";
 // import DisplayData from "./Displaying-data/DisplayData";
 //import Condition from "./Conditional-rendering/Condition";
-// import Hooks from "./Using-Hooks/Hooks";
 import Game from "./Tic-Tac-Toe/Game";
 import Container from "react-bootstrap/Container";
 function App() {
