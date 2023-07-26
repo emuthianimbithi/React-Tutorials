0db0b7823030d6cdddd597bd8e8b219a30f0e824
emuthianimbithi <138307211+emuthianimbithi@users.noreply.github.com>
2023-07-26 16:56:48 +0300

    index comment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 //some comment
+//added a comment
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
